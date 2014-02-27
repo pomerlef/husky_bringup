a3ce9003c5c49b4f0ad89dd865f12637761641ef
Francois Pomerleau <f.pomerleau@gmail.com>
2014-02-27 18:16:58 -0500

    add steering efficiency capabilities

--- nodes/dead_reckoning.py
+++ nodes/dead_reckoning.py
@@ -1,32 +1,34 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('husky_bringup')
-import roslib.rosenv
+#import roslib.rosenv
 import rospy
 import PyKDL
 import copy
 
-from math import sin,cos
+from math import sin,cos,pi
 from geometry_msgs.msg import Quaternion
 from sensor_msgs.msg import Imu
 from nav_msgs.msg import Odometry
+from std_srvs.srv import Empty, EmptyResponse
 from clearpath_base.msg import Encoders
 
 # Dynamic Reconfigure
 import dynamic_reconfigure.server
 from husky_bringup.cfg import HuskyConfig
 
-ODOM_POSE_COVAR_MOTION = [1e-3, 0, 0, 0, 0, 0, 
-                        0, 1e-3, 0, 0, 0, 0,
-                        0, 0, 1e6, 0, 0, 0,
-                        0, 0, 0, 1e6, 0, 0,
-                        0, 0, 0, 0, 1e6, 0,
-                        0, 0, 0, 0, 0, 1e6]
-ODOM_POSE_COVAR_NOMOVE = [1e-9, 0, 0, 0, 0, 0, 
-                         0, 1e-3, 1e-9, 0, 0, 0,
-                         0, 0, 1e6, 0, 0, 0,
-                         0, 0, 0, 1e6, 0, 0,
-                         0, 0, 0, 0, 1e6, 0,
-                         0, 0, 0, 0, 0, 1e-9]
+ODOM_POSE_COVAR_MOTION = [1e-3,     0,   0,    0,    0,   0, 
+                             0, 1e-3,    0,    0,    0,   0,
+                             0,     0, 1e6,    0,    0,   0,
+                             0,     0,   0,  1e6,    0,   0,
+                             0,     0,   0,    0,  1e6,   0,
+                             0,     0,   0,    0,    0, 1e-2]
+
+ODOM_POSE_COVAR_NOMOVE = [1e-9,    0,    0,   0,   0,    0, 
+                             0, 1e-3,    0,   0,   0,    0,
+                             0,    0,  1e6,   0,   0,    0,
+                             0,    0,    0, 1e6,   0,    0,
+                             0,    0,    0,   0, 1e6,    0,
+                             0,    0,    0,   0,   0, 1e-9]
 
 ODOM_TWIST_COVAR_MOTION = [1e-3, 0, 0, 0, 0, 0, 
                          0, 1e-3, 0, 0, 0, 0,
@@ -35,7 +37,7 @@ ODOM_TWIST_COVAR_MOTION = [1e-3, 0, 0, 0, 0, 0,
                          0, 0, 0, 0, 1e6, 0,
                          0, 0, 0, 0, 0, 1e6]
 ODOM_TWIST_COVAR_NOMOVE = [1e-9, 0, 0, 0, 0, 0, 
-                          0, 1e-3, 1e-9, 0, 0, 0,
+                          0, 1e-3, 0, 0, 0, 0,
                           0, 0, 1e6, 0, 0, 0,
                           0, 0, 0, 1e6, 0, 0,
                           0, 0, 0, 0, 1e6, 0,
@@ -48,12 +50,19 @@ class DeadReckoning(object):
         # Parameters
         self.width = rospy.get_param('~width',0.5)
         self.gyro_scale = rospy.get_param('~gyro_scale_correction',1.0)
+        self.steering_efficiency= rospy.get_param('~steering_efficiency',0.5)
 
         # Set up publishers/subscribers
         self.pub_imu = rospy.Publisher('imu_data',Imu)
         self.pub_enc_odom = rospy.Publisher('encoder',Odometry);
         rospy.Subscriber('imu/data', Imu, self.HandleIMU)
         rospy.Subscriber('husky/data/encoders', Encoders, self.HandleEncoder)
+
+        # Set up services
+        rospy.Service('start_steering_efficiency_calib', Empty, self.HandleStartCalibSteering)
+        rospy.Service('stop_steering_efficiency_calib', Empty, self.HandleStopCalibSteering)
+
+        self.calibStarted = False
 
         # Initialize odometry message
         self.odom = Odometry()
@@ -63,15 +72,38 @@ class DeadReckoning(object):
 
         dynamic_reconfigure.server.Server(HuskyConfig, self.Reconfigure)
 
+    def HandleStartCalibSteering(self, req):
+        rospy.loginfo("Starting steering calibration");
+        rospy.loginfo("Rotate the robot by a full turn");
+        self.fullTurn = 0.0
+        self.calibStarted = True
+        return EmptyResponse()
+
+    def HandleStopCalibSteering(self, req):
+
+        if not self.calibStarted:
+          rospy.loginfo("You need to start the calibration first");
+        else:
+          self.calibStarted = False
+          if not self.fullTurn == 0.0:
+            self.steering_efficiency = abs(2.0*pi/self.fullTurn)
+            rospy.loginfo("The new steering efficiency is %.3f" % self.steering_efficiency);
+          else:
+            rospy.loginfo("The robot didn't turned. Not modification to the steering efficiency will be applied");
+        return EmptyResponse()
+
+
+
+
     def Reconfigure(self, config, level):
         # Handler for dynamic_reconfigure
         self.gyro_scale = config['gyro_scale_correction']
         return config
 
     def HandleIMU(self,data):
-	# Correct IMU data
-	# Right now, gyro scale only
-	# TODO: Evaluate necessity of adding drift correction 
+        # Correct IMU data
+        # Right now, gyro scale only
+        # TODO: Evaluate necessity of adding drift correction 
         imu_corr = copy.deepcopy(data)
         imu_corr.header.frame_id = "base_link"
         imu_corr.angular_velocity.z = data.angular_velocity.z * self.gyro_scale
@@ -86,15 +118,21 @@ class DeadReckoning(object):
         dr = ((data.encoders[0].travel - self.last_encoder.encoders[0].travel) +
               (data.encoders[1].travel - self.last_encoder.encoders[1].travel)) / 2;
         da = ((data.encoders[1].travel - self.last_encoder.encoders[1].travel) - 
-              (data.encoders[0].travel - self.last_encoder.encoders[0].travel)) / self.width;
+              (data.encoders[0].travel - self.last_encoder.encoders[0].travel)) * \
+              (self.steering_efficiency / self.width);
         ddr = (data.encoders[0].speed + data.encoders[1].speed)/2;
-        dda = (data.encoders[1].speed - data.encoders[0].speed)/self.width;
+        dda = (data.encoders[1].speed - data.encoders[0].speed) * \
+              (self.steering_efficiency / self.width);
         self.last_encoder = data
+
+        # Integrate for calibration
+        if self.calibration:
+          self.fullTurn = self.fullTurn + da
 
         # Update data
         o = self.odom.pose.pose.orientation
         cur_heading = PyKDL.Rotation.Quaternion(o.x,o.y,o.z,o.w).GetEulerZYX()
-        self.odom.pose.pose.position.x += dr * cos(cur_heading[0])
+        self.odom.pose.pose.position.x += dr * cos(cur_heading[0]) # FP: TODO confirm that
         self.odom.pose.pose.position.y += dr * sin(cur_heading[0]) 
         quat = PyKDL.Rotation.RotZ(cur_heading[0] + da).GetQuaternion()
         self.odom.pose.pose.orientation = Quaternion(quat[0],quat[1],quat[2],quat[3])
