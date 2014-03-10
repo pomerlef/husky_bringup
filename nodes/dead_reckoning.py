ef920a561617819adee3a3992ee0218f5f134344
Administrator <administrator@cpr-lav01.cpr-lav01>
2014-03-10 16:21:12 -0400

    adjust the value of steering_efficiency

--- nodes/dead_reckoning.py
+++ nodes/dead_reckoning.py
@@ -45,12 +45,21 @@ ODOM_TWIST_COVAR_NOMOVE = [1e-9, 0, 0, 0, 0, 0,
 
 
 class DeadReckoning(object):
+
+    def InitOdometry(self):
+        self.odom = Odometry()
+        self.odom.header.frame_id = "odom_combined"
+        self.odom.child_frame_id = "base_footprint" 
+        self.last_encoder = []
+
+
     def __init__(self):
         rospy.init_node('dead_reckoning')
         # Parameters
         self.width = rospy.get_param('~width',0.5)
         self.gyro_scale = rospy.get_param('~gyro_scale_correction',1.0)
-        self.steering_efficiency= rospy.get_param('~steering_efficiency',0.5)
+        # a steerign efficiency of 0.48 is typical for indoor floors
+        self.steering_efficiency= rospy.get_param('~steering_efficiency',0.48)
 
         # Set up publishers/subscribers
         self.pub_imu = rospy.Publisher('imu_data',Imu)
@@ -59,19 +68,18 @@ class DeadReckoning(object):
         rospy.Subscriber('husky/data/encoders', Encoders, self.HandleEncoder)
 
         # Set up services
-        rospy.Service('start_steering_efficiency_calib', Empty, self.HandleStartCalibSteering)
-        rospy.Service('stop_steering_efficiency_calib', Empty, self.HandleStopCalibSteering)
+        rospy.Service('~start_steering_calib', Empty, self.HandleStartCalibSteering)
+        rospy.Service('~stop_steering_calib', Empty, self.HandleStopCalibSteering)
+        rospy.Service('~reset_odometry', Empty, self.HandleResetOdom)
 
         self.calibStarted = False
 
         # Initialize odometry message
-        self.odom = Odometry()
-        self.odom.header.frame_id = "odom_combined"
-        self.odom.child_frame_id = "base_footprint" 
-        self.last_encoder = []
-
+        self.InitOdometry()
+        
         dynamic_reconfigure.server.Server(HuskyConfig, self.Reconfigure)
-
+    
+    
     def HandleStartCalibSteering(self, req):
         rospy.loginfo("Starting steering calibration");
         rospy.loginfo("Rotate the robot by a full turn");
@@ -86,13 +94,16 @@ class DeadReckoning(object):
         else:
           self.calibStarted = False
           if not self.fullTurn == 0.0:
-            self.steering_efficiency = abs(2.0*pi/self.fullTurn)
+            correction = abs(2.0*pi/self.fullTurn)
+            self.steering_efficiency = self.steering_efficiency * correction
             rospy.loginfo("The new steering efficiency is %.3f" % self.steering_efficiency);
           else:
             rospy.loginfo("The robot didn't turned. Not modification to the steering efficiency will be applied");
         return EmptyResponse()
 
-
+    def HandleResetOdom(self, req):
+        self.InitOdometry()
+        return EmptyResponse()
 
 
     def Reconfigure(self, config, level):
@@ -132,8 +143,8 @@ class DeadReckoning(object):
         # Update data
         o = self.odom.pose.pose.orientation
         cur_heading = PyKDL.Rotation.Quaternion(o.x,o.y,o.z,o.w).GetEulerZYX()
-        self.odom.pose.pose.position.x += dr * cos(cur_heading[0]) # FP: TODO confirm that
-        self.odom.pose.pose.position.y += dr * sin(cur_heading[0]) 
+        self.odom.pose.pose.position.x += dr * cos(cur_heading[0] + da/2)
+        self.odom.pose.pose.position.y += dr * sin(cur_heading[0] + da/2) 
         quat = PyKDL.Rotation.RotZ(cur_heading[0] + da).GetQuaternion()
         self.odom.pose.pose.orientation = Quaternion(quat[0],quat[1],quat[2],quat[3])
         self.odom.twist.twist.linear.x = ddr
