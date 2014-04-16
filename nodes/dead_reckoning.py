b0b705920d491ebb8df31fca34f1ce65c4b78b0e
Francois Pomerleau <f.pomerleau@gmail.com>
2014-04-16 17:46:55 -0400

    patch for encoder jump

--- nodes/dead_reckoning.py
+++ nodes/dead_reckoning.py
@@ -111,6 +111,8 @@ class DeadReckoning(object):
    
     def HandleEncoder(self,data):
         # Initialize encoder state
+        print self.last_encoder
+
         if not self.last_encoder:
             self.last_encoder = data
             return
@@ -129,28 +131,35 @@ class DeadReckoning(object):
         if self.calibration:
           self.fullTurn = self.fullTurn + da
 
-        # Update data
-        o = self.odom.pose.pose.orientation
-        cur_heading = PyKDL.Rotation.Quaternion(o.x,o.y,o.z,o.w).GetEulerZYX()
-        self.odom.pose.pose.position.x += dr * cos(cur_heading[0]) # FP: TODO confirm that
-        self.odom.pose.pose.position.y += dr * sin(cur_heading[0]) 
-        quat = PyKDL.Rotation.RotZ(cur_heading[0] + da).GetQuaternion()
-        self.odom.pose.pose.orientation = Quaternion(quat[0],quat[1],quat[2],quat[3])
-        self.odom.twist.twist.linear.x = ddr
-        self.odom.twist.twist.angular.z = dda
+        # FP: this is a patch for encoder msg reseting
+        # It is not a solution, the problem needs to be solved at a lower level
         
-        self.odom.header.stamp = rospy.Time.now()
+        rospy.loginfo("dr: %f" % dr)
 
-        # If our wheels aren't moving, we're likely not moving at all.
-        # Adjust covariance appropriately
-        if data.encoders[0].speed == 0 and data.encoders[1].speed == 0:
-            self.odom.pose.covariance = ODOM_POSE_COVAR_NOMOVE
-            self.odom.twist.covariance = ODOM_TWIST_COVAR_NOMOVE
-        else:
-            self.odom.pose.covariance = ODOM_POSE_COVAR_MOTION
-            self.odom.twist.covariance = ODOM_TWIST_COVAR_MOTION
+        if(abs(dr) > 2.0):
+          # Update data
+          o = self.odom.pose.pose.orientation
+          cur_heading = PyKDL.Rotation.Quaternion(o.x,o.y,o.z,o.w).GetEulerZYX()
+          self.odom.pose.pose.position.x += dr * cos(cur_heading[0]) # FP: TODO confirm that
+          self.odom.pose.pose.position.y += dr * sin(cur_heading[0]) 
+          quat = PyKDL.Rotation.RotZ(cur_heading[0] + da).GetQuaternion()
+          self.odom.pose.pose.orientation = Quaternion(quat[0],quat[1],quat[2],quat[3])
+          self.odom.twist.twist.linear.x = ddr
+          self.odom.twist.twist.angular.z = dda
+          
+          self.odom.header.stamp = rospy.Time.now()
 
-        self.pub_enc_odom.publish(self.odom)
+          # If our wheels aren't moving, we're likely not moving at all.
+          # Adjust covariance appropriately
+          if data.encoders[0].speed == 0 and data.encoders[1].speed == 0:
+              self.odom.pose.covariance = ODOM_POSE_COVAR_NOMOVE
+              self.odom.twist.covariance = ODOM_TWIST_COVAR_NOMOVE
+          else:
+              self.odom.pose.covariance = ODOM_POSE_COVAR_MOTION
+              self.odom.twist.covariance = ODOM_TWIST_COVAR_MOTION
+
+          self.pub_enc_odom.publish(self.odom)
+
 
 if __name__ == "__main__":
     obj = DeadReckoning()
